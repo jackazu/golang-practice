3577b931566d1cf127191eac57b022f263ccd94b
jackazu <48099418+jackazu@users.noreply.github.com>
2020-04-24 05:58:52 +0900

    Add anser of practice1.4

--- ch01/Practice4.go
+++ ch01/Practice4.go
@@ -3,14 +3,16 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
-func main() {
+func practice4() {
 	counts := make(map[string]int)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -25,8 +27,12 @@ func main() {
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
+			for _, name := range searchDuplicateLineHaveFile(files, counts) {
+				fmt.Println(name)
+			}
 		}
 	}
+
 }
 
 func countLines(f *os.File, counts map[string]int) {
@@ -34,4 +40,23 @@ func countLines(f *os.File, counts map[string]int) {
 	for input.Scan() {
 		counts[input.Text()]++
 	}
+}
+
+func searchDuplicateLineHaveFile(files []string, counts map[string]int) []string {
+	duplicateLineHaveFileNames := []string{}
+	for _, fileName := range files {
+		text, err := ioutil.ReadFile(fileName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(text), "\n") {
+			if counts[line] > 1 {
+				duplicateLineHaveFileNames = append(duplicateLineHaveFileNames, fileName)
+				break
+			}
+		}
+	}
+
+	return duplicateLineHaveFileNames
 }
