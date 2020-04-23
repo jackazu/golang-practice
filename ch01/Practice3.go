62ed68b59e2db45742736a57414f08795d5371b3
jackazu <48099418+jackazu@users.noreply.github.com>
2020-04-24 02:37:39 +0900

    Add anser of Practice1.3

---
+++ ch01/Practice3.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	var inefficiencyExeTime = inefficiency()
+	var efficiencyExeTime = efficiency()
+	fmt.Printf("%d - %d = %dμs \n", inefficiencyExeTime, efficiencyExeTime, inefficiencyExeTime-efficiencyExeTime)
+}
+
+func efficiency() int64 {
+	start := time.Now()
+
+	fmt.Println(strings.Join(os.Args[1:], " "))
+
+	return time.Since(start).Microseconds()
+}
+
+func inefficiency() int64 {
+	start := time.Now()
+
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+
+	return time.Since(start).Microseconds()
+}
